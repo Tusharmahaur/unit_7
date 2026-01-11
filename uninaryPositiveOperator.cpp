ccf89f03726615df4e1d0cdcceafeb36215c4947
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-11 16:01:18 +0530

    this

---
+++ uninaryPositiveOperator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class demo{
+    int data;
+    friend void operator+(demo &);
+public:
+    demo(int d){
+        data = d;
+    }
+    void show(){
+        cout<<"\nValue = "<<data;
+    }
+};
+
+void operator+(demo &D){
+    D.data += D.data;
+}
+int main(){
+    demo D1(10);
+    cout<<"\nBefore unary + operator ";
+    D1.show();
+    +D1; // unary + operator called
+    cout<<"\nAfter unary + operator ";
+    D1.show();
+    return 0;
+}
